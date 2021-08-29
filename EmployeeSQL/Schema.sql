fdec3b0c4e74ba7643ea9f1d92fc6230ba3251f3
Tyler Cutrer <wtcutrer@gmail.com>
2021-08-29 12:11:17 -0500

    Updated Primary and Foreign Keys

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS departments
+DROP TABLE IF EXISTS employees
 DROP TABLE IF EXISTS dep_employees
 DROP TABLE IF EXISTS dep_manager
-DROP TABLE IF EXISTS employees
 DROP TABLE IF EXISTS salaries
 DROP TABLE IF EXISTS titles
 
-CREATE TABLE departments (
+CREATE TABLE departments(
     dept_no VARCHAR(50) NOT NULL,
     dept_name VARCHAR(50) NOT NULL,
 	PRIMARY KEY (dept_no)
@@ -13,42 +13,49 @@ CREATE TABLE departments (
 
 Select * from departments
 
-CREATE TABLE dep_employees (
-    emp_no integer NOT NULL,
-    dept_no VARCHAR(50) NOT NULL
-);
-
-Select * from dep_employees
-
-CREATE TABLE dep_manager (
-    dept_no VARCHAR(50) NOT NULL,
-	emp_no integer NOT NULL
-);
-
-Select * from dep_manager
-
-CREATE TABLE employees (
+CREATE TABLE employees(
     emp_no integer NOT NULL,
     emp_title VARCHAR(50) NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	sex VARCHAR(50) NOT NULL,
-	hire_date DATE NOT NULL
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
 select * from employees
 
-CREATE TABLE salaries (
+CREATE TABLE dep_employees(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(50) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+Select * from dep_employees
+
+CREATE TABLE dep_manager(
+    dept_no VARCHAR(50) NOT NULL,
 	emp_no integer NOT NULL,
-	salary integer NOT NULL
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+Select * from dep_manager
+
+CREATE TABLE salaries(
+	emp_no integer NOT NULL,
+	salary integer NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 select * from salaries
 
-CREATE TABLE titles (
+CREATE TABLE titles(
 	title_id VARCHAR(50) NOT NULL,
-	title VARCHAR(50) NOT NULL
+	title VARCHAR(50) NOT NULL,
+	PRIMARY KEY (title_id)
 );
 
 Select * from titles
